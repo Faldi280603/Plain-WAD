5e14bbbd632f8c01f14bf1f942ad579d1421a096
Faldi280603 <mrizkrifaldi28@gmail,com>
2023-10-21 12:50:45 +0700

    PLAIN TUGAS 2

---
+++ models/user.php
@@ -0,0 +1,29 @@
+<?php
+namespace Models;
+
+class User {
+    private $id;
+    private $username;
+    private $photo;
+
+    public function __construct($id, $username, $photo) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->photo = $photo;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    // Tambahan untuk menampilkan photo user yang telah login (masih error)
+    // public function getPhoto() {
+    //     return $this->photo;
+    // }
+
+}
+?>
